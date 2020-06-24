b68e49b28bcefa4de37030185ab10652d606be05
Pavel Zaitsev <z.pasha00@gmail.com>
2020-06-24 17:07:54 +0300

    aaaa

--- app/controllers/authcontroller.js
+++ app/controllers/authcontroller.js
@@ -4,10 +4,26 @@ exports.signup = function(req, res) {
 
     res.render('signup');
 
-}
+};
 
 exports.signin = function(req, res) {
 
-    res.render('signin');
+        res.render('signin');
 
-}
+};
+
+exports.dashboard = function(req, res) {
+
+    res.render('dashboard');
+
+};
+
+exports.logout = function(req, res) {
+
+    req.session.destroy(function(err) {
+
+        res.redirect('/');
+
+    });
+
+};
--- server.js
+++ server.js
@@ -1,42 +1,63 @@
-var express = require('express');
-var app = express();
-var passport   = require('passport')
-var session    = require('express-session')
+var express = require('express')
+var app = express()
+var passport = require('passport')
+var session = require('express-session')
 var bodyParser = require('body-parser')
+var env = require('dotenv')
 var exphbs = require('express-handlebars')
-var env = require('dotenv');
+
 
 //For BodyParser
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
+
 // For Passport
-app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+})); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 
+
+//For Handlebars
+app.set('views', './app/views')
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+
+
 app.get('/', function(req, res) {
 
-    res.send('Welcome');
-
-});
-
-
-app.listen(3000, function(err) {
-
-    if (!err)
-        console.log("Site is live");
-    else console.log(err)
+    res.send('Welcome to Passport with Sequelize');
 
 });
 
 //Models
 var models = require("./app/models");
 
+//Routes
+
+var authRoute = require('./app/routes/auth.js')(app,passport);
+
+
+//load passport strategies
+
+require('./app/config/passport/passport.js')(passport, models.user);
+
+
 //Sync Database
+
 models.sequelize.sync().then(function() {
 
     console.log('Nice! Database looks fine')
+
 
 }).catch(function(err) {
 
@@ -44,12 +65,13 @@ models.sequelize.sync().then(function() {
 
 });
 
-//For Handlebars
-app.set('views', '/Users/moonman/Desktop/ISTP-prekol/rest-api/app/views')
-app.engine('hbs', exphbs({
-    extname: '.hbs'
-}));
-app.set('view engine', '.hbs');
 
-//Routes
-var authRoute = require('./app/routes/auth.js')(app);
+app.listen(3000, function(err) {
+
+    if (!err)
+
+        console.log("Site is live");
+
+    else console.log(err)
+
+});
